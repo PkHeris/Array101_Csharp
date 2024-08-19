c9e4ecb6ff4d6d4553479d069785d70901e8a53e
pkheris <kazemzadehp@gmail.com>
2024-08-19 07:23:13 -0700

    changed

--- FindNumbers.cs
+++ FindNumbers.cs
@@ -23,7 +23,7 @@ namespace Array101
             int digitCount = 0;
             while (num != 0)
             {
-                num = num / 10;
+                num /= 10;
                 digitCount++;
             }
 
