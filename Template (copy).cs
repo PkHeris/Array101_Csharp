1f6f8b760e2d26f8b062eded834fff4679ebeb57
pkheris <kazemzadehp@gmail.com>
2024-08-24 19:35:16 -0700

    fhgzxc

---
+++ Template (copy).cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Array101
+{
+    public class ProblemName
+    {
+        public int SolveProblem(int[] nums)
+        {
+            return result;
+        }
+    }
+}
+
