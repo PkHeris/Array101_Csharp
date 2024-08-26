83734033ff2ae1a9fea8144d8b43d012785e46eb
PkHeris <pkazemzadehheris@uwaterloo.ca>
2024-08-26 16:29:45 -0700

    sfdsd

---
+++ MoveZeroes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Array101
+{
+    public class MoveZeroes
+    {
+        public void MoveZeroesInPlace(int[] nums)
+        {
+            int n = nums.Length;
+            int writer_ind = 0;
+            for (int reader = 0; reader < n; reader++)
+            {
+                if (nums[reader] != 0)
+                {
+                    if(writer_ind != reader)
+                    {
+                        nums[writer_ind] = nums[reader];
+                    }
+                    
+                    writer_ind++;
+                }
+            }
+            for (int i = writer_ind; i < n; i++)
+            {
+                nums[i] = 0;
+            }
+        }
+    }
+}
+
