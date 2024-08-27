62ead5729e345b0c8ba11605f4aca156ed62aee0
PkHeris <pkazemzadehheris@uwaterloo.ca>
2024-08-27 14:58:27 -0700

    RemoveElementK, test not implemented

---
+++ RemoveElementK.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Array101
+{
+    public class RemoveElementK
+    {
+        public int RemoveElementKInPlace(int[] nums, int val)
+        {
+            int wrt = 0;
+            for (int i = 0; i<nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    if(i != wrt)
+                    {
+                        nums[wrt] = nums[i];
+                    }
+                    wrt++;
+                }
+            }
+            return wrt;
+        }
+       
+    }
+}
+
